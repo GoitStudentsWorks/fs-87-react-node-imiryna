05b93e5ea82ae521aca927d53f133f342d7bb9a4
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-02-08 20:43:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="WaterTracker">
+      <BrowserRouter basename="fs-87-react-node-imiryna">
         <App />
       </BrowserRouter>
     </PersistGate>
